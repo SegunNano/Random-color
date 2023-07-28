a57b99a321c2094d3e3680b0ffc23a053f863db9
SegunNano <segunfadipe97@gmail.com>
2023-07-28 01:35:16 +0100

    zxvdvs

---
+++ node.js
@@ -0,0 +1,16 @@
+// console.log('How are you?');
+const button = document.querySelector('button');
+const h1 = document.querySelector('h1')
+button.addEventListener('click', function() {
+   const r = Math.floor(Math.random() * 255);
+   const g = Math.floor(Math.random() * 255);
+   const b = Math.floor(Math.random() * 255);
+   const newColor = `rgb(${r}, ${g}, ${b})`;
+   document.body.style.backgroundColor = newColor;
+   h1.innerText = newColor
+   if (r < 60 || g < 60 || b < 60) {
+    h1.style.color = 'white'
+   } else {
+    h1.style.color = 'black'
+   }
+})
