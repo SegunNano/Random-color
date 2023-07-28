ea25bc43a82dfc64330b35f770f0f2429dd576fc
SegunNano <segunfadipe97@gmail.com>
2023-07-28 01:43:46 +0100

    improved

--- node.js
+++ node.js
@@ -7,10 +7,13 @@ button.addEventListener('click', function() {
    const b = Math.floor(Math.random() * 255);
    const newColor = `rgb(${r}, ${g}, ${b})`;
    document.body.style.backgroundColor = newColor;
+   button.style.color = newColor
    h1.innerText = newColor
    if (r < 60 || g < 60 || b < 60) {
     h1.style.color = 'white'
+    button.style.backgroundColor = 'white'
    } else {
     h1.style.color = 'black'
+    button.style.backgroundColor = 'black'
    }
 })
