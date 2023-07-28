9e1f910030c541e671bc3b38c219288cab9e7c6c
SegunNano <segunfadipe97@gmail.com>
2023-07-28 02:08:15 +0100

    cOLOR PALETTE

--- node.js
+++ node.js
@@ -1,6 +1,9 @@
 // console.log('How are you?');
 const button = document.querySelector('button');
-const h1 = document.querySelector('h1')
+const h1 = document.querySelector('h1');
+const red = document.querySelector('#red');
+const green = document.querySelector('#green');
+const blue = document.querySelector('#blue');
 button.addEventListener('click', function() {
    const r = Math.floor(Math.random() * 255);
    const g = Math.floor(Math.random() * 255);
@@ -8,8 +11,11 @@ button.addEventListener('click', function() {
    const newColor = `rgb(${r}, ${g}, ${b})`;
    document.body.style.backgroundColor = newColor;
    button.style.color = newColor
-   h1.innerText = newColor
-   if (r < 60 || g < 60 || b < 60) {
+   red.style.backgroundColor = `rgb(${r}, 0, 0)`;
+   green.style.backgroundColor = `rgb(0, ${g}, 0)`;
+   blue.style.backgroundColor = `rgb(0, 0, ${b})`;
+   h1.innerText = newColor.toUpperCase()
+   if (r < 70 || g < 70 || b < 70) {
     h1.style.color = 'white'
     button.style.backgroundColor = 'white'
    } else {
